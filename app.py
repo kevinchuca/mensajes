3bf7c0254c0ffdce44cdca0d9e2fd0c066cd8a64
kevinchuca <kevinncito12@gmail.com>
2026-01-06 10:20:24 -0400

    Forzando la conexion con bolprex_app

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from bolprex_app import create_app
-
 app = create_app()
-
 if __name__ == "__main__":
     app.run()
-
-# Cambio para forzar el despliegue 1.0
+    
+# Intento final de conexion    
