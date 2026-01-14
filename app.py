70ed895023c9235f5578c8fb0ad69efaa95a9231
kevinchuca <kevinncito12@gmail.com>
2026-01-14 10:47:34 -0400

    Forzar recreacion de tablas para añadir client_name

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from bolprex_app import create_app, db # Asegúrate de importar 'db' también
+from bolprex_app import create_app, db # Asegúrate de importar db
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        # Estas dos líneas borran todo y crean la base de datos de nuevo
+        # ESTO ES LO QUE SOLUCIONA EL ERROR:
+        print("Borrando y recreando tablas...")
         db.drop_all() 
         db.create_all()
-        print("Base de datos actualizada con éxito")
+        print("Base de datos actualizada con la columna client_name")
+    
     app.run()
