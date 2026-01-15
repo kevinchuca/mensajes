c5569fb7aef4056686b6066d23d2197b4e6aa976
kevinchuca <kevinncito12@gmail.com>
2026-01-14 21:45:07 -0400

    Corrigiendo endpoint en shipment_detail.html

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-from bolprex_app import create_app, db
-from sqlalchemy import text
+from bolprex_app import create_app
+# Ya no necesitamos importar 'db' ni 'text' aquí
 
 app = create_app()
-
-# Este bloque se ejecutará al iniciar en Render
-with app.app_context():
-    try:
-        # Intentamos añadir la columna faltante directamente
-        db.session.execute(text('ALTER TABLE shipment ADD COLUMN client_name VARCHAR(120)'))
-        db.session.commit()
-        print("Columna client_name añadida con éxito.")
-    except Exception as e:
-        # Si la columna ya existe, simplemente ignoramos el error
-        db.session.rollback()
-        print(f"Aviso: {e}")
 
 if __name__ == "__main__":
     app.run()
