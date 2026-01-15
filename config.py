2a3311c4f1a64c52db648a39e13c624a6481a802
kevinchuca <kevinncito12@gmail.com>
2026-01-14 23:27:10 -0400

    Activando cloudinary y corrigiendo DB URL

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
-# 1. ACTIVACIÓN DE CLOUDINARY (Faltaba esto para evitar el Error 500)
+# ESTO ACTIVA TUS LLAVES DE RENDER
 cloudinary.config( 
     cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'), 
     api_key = os.getenv('CLOUDINARY_API_KEY'), 
@@ -17,7 +17,7 @@ cloudinary.config(
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
     
-    # 2. CORRECCIÓN DE LA URL DE BASE DE DATOS PARA RENDER
+    # Arreglo de base de datos para Render
     db_url = os.getenv("DATABASE_URL")
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
@@ -26,4 +26,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", str(BASE_DIR / "static" / "uploads"))
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "another-dev-key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-key")
